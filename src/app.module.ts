f0cca4205126c7146811f1535abb3da971359b93
Walther Seidel <waseidel@gmail.com>
2023-04-17 07:44:13 -0500

    Updating main

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,8 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'etm',
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
-    AuthModule,
-  ],
-  controllers: [],
-  providers: [],
+  imports: [CommonModule, UsersModule],
 })
 export class AppModule {}
